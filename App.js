2cf0acdf85c89a7688aca8eed1ac13e9d1fea81c
Katherine Hambley <superdesigngirl@mac.com>
2023-10-30 15:49:29 -0500

    4.3.9 added Tab bar on bottom

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
 import LoginScreen from './LoginScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -15,10 +18,29 @@ export default function App() {
         <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Navigator>
+          <Tab.Navigator initialRouteName='Login'
+              screenOptions={({ route }) => ({
+               tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if(route.name === 'Welcome') {
+                  // https://icons.expo.fyi/Index
+                  iconName = focused ? 'ios-home-sharp' : 'ios-home-outline';
+                }
+                else if (route.name === 'Login') {
+                  iconName = 'ios-log-in';
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />
+
+               },
+               tabBarActiveTintColor: 'tomato',
+               tabBarInactiveTintColor: 'gray',
+               
+              })}>
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Login" component={LoginScreen} />
+          </Tab.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
